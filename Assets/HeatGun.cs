97445b421f414fae061b3fa55c3f82e7eed6508f
Casey Manning <kcgrind@gmail.com>
2022-05-07 23:52:55 -0700

    vertex paint sorta works

--- Assets/HeatGun.cs
+++ Assets/HeatGun.cs
@@ -6,6 +6,8 @@ public class HeatGun : MonoBehaviour
 {
     LineRenderer lr;
 
+    float darkenRange = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +21,37 @@ public class HeatGun : MonoBehaviour
         RaycastHit hit;
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
-        print(transform.position);
 
         if (Physics.Raycast(transform.position, transform.right, out hit, 100, layerMask))
         {
             lr.SetPosition(1, hit.point);
+
+            GameObject ball = hit.transform.gameObject;
+            if (ball.CompareTag("Object"))
+            {
+                Mesh m = ball.GetComponent<MeshFilter>().mesh;
+                Vector3[] vertices = m.vertices;
+
+                Color[] colors = new Color[vertices.Length];
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    if(Vector3.Distance(hit.point, ball.transform.TransformPoint(vertices[i])) < darkenRange)
+                    {
+                        colors[i] = Color.red;
+                    } else
+                    {
+                        if (m.colors.Length > i)
+                        {
+                            colors[i] = m.colors[i];
+                        } else
+                        {
+                            colors[i] = new Color(255, 250, 250);
+                        }
+                    }
+                }
+                m.colors = colors;
+            }
+
         } else
         {
             lr.SetPosition(1, transform.position);
